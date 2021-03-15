a6e0e4881460c807b9388e3d079d1e3dece19c6f
SH-OH <dhtkdgh429@gmail.com>
2021-03-15 18:51:15 +0900

    Only Extensions

--- Package.swift
+++ Package.swift
@@ -11,27 +11,15 @@ let package = Package(
     products: [
         .library(
             name: "SPMSHOHProxy",
-            type: .dynamic,
             targets: ["SPMSHOHProxy"]
         )
     ],
     dependencies: [
-        .package(url: "https://github.com/ReactorKit/ReactorKit.git", .upToNextMajor(from: "2.0.0")),
-        .package(url: "https://github.com/ReactiveX/RxSwift.git", .upToNextMajor(from: "5.0.0")),
-        .package(url: "https://github.com/RxSwiftCommunity/RxDataSources.git", .upToNextMajor(from: "4.0.0")),
-        .package(url: "https://github.com/Moya/Moya.git", .upToNextMajor(from: "14.0.0")),
-        .package(url: "https://github.com/onevcat/Kingfisher.git", .upToNextMajor(from: "6.0.0"))
     ],
     targets: [
         .target(
             name: "SPMSHOHProxy",
             dependencies: [
-                "ReactorKit",
-                "RxSwift",
-                .product(name: "RxCocoa", package: "RxSwift"),
-                "RxDataSources",
-                .product(name: "RxMoya", package: "Moya"),
-                "Kingfisher"
             ]
         ),
         .testTarget(
--- Sources/SPMSHOHProxy/Extensions/ObservableType+Extensions.swift
+++
@@ -1,9 +0,0 @@
-import Foundation
-import RxSwift
-import RxCocoa
-
-extension ObservableType {
-    public func asDriverOnEmpty() -> Driver<Element> {
-        return asDriver(onErrorDriveWith: .empty())
-    }
-}
