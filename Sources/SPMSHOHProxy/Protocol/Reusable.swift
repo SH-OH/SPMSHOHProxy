b1812a4685c53bbbff1b91b1770ecf04f55cb760
SH-OH <dhtkdgh429@gmail.com>
2021-03-15 16:44:00 +0900

    Update Extensions

---
+++ Sources/SPMSHOHProxy/Protocol/Reusable.swift
@@ -0,0 +1,11 @@
+import Foundation
+
+public protocol Reusable: class {
+    static var reuseIdentifier: String { get }
+}
+
+extension Reusable {
+    public static var reuseIdentifier: String {
+        return String(describing: self)
+    }
+}
