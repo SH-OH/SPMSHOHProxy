d5ca0e7304475f40939d1ffcae9fa225d7800e32
Jayden(오상호) <dhtkdgh429@gmail.com>
2021-10-22 01:22:26 +0900

    Add Dependency

---
+++ Sources/SPMSHOHProxy/Protocol/Dependency.swift
@@ -0,0 +1,12 @@
+//
+//  File.swift
+//  
+//
+//  Created by Jayden OH on 2021/10/22.
+//
+
+import Foundation
+
+public protocol Dependency: AnyObject {}
+
+public protocol EmptyDependency: Dependency {}
