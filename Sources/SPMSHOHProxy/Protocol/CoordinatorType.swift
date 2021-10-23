14cae3e7bd0ef598e6421219a93ea8d47dbad208
Jayden(오상호) <dhtkdgh429@gmail.com>
2021-10-24 02:57:45 +0900

    Fix CoordinatorType

--- Sources/SPMSHOHProxy/Protocol/CoordinatorType.swift
+++ Sources/SPMSHOHProxy/Protocol/CoordinatorType.swift
@@ -1,20 +1,16 @@
 import UIKit.UINavigationController
 
+public protocol NavigationType {}
+
 public protocol CoordinatorType {
     func start(with dependency: DependencyType?)
-    func coordinate(to coordinator: CoordinatorType, with dependency: DependencyType?)
+    func navigate(to navigation: NavigationType, with dependency: DependencyType?)
 }
 
 extension CoordinatorType {
-    public func coordinate(to coordinator: CoordinatorType, with dependency: DependencyType?) {
-        coordinator.start(with: dependency)
-    }
+    public func navigate(to navigation: NavigationType, with dependency: DependencyType?) {}
 }
 
-public protocol NavigationType {}
-
 public protocol Coordinatable {
-    associatedtype Coordinator: CoordinatorType
-    var coordinator: Coordinator? { get }
-    func navigate(to navigation: NavigationType, with dependency: DependencyType?)
+    var coordinator: CoordinatorType? { get }
 }
