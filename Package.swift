a31c491c97836c53b9595e5852526953bc8f28e7
Jayden(오상호) <dhtkdgh429@gmail.com>
2021-10-26 19:46:21 +0900

    Remove macos

--- Package.swift
+++ Package.swift
@@ -6,7 +6,7 @@ import PackageDescription
 let package = Package(
     name: "SPMSHOHProxy",
     platforms: [
-        .iOS(.v10), .macOS(.v11)
+        .iOS(.v10)
     ],
     products: [
         .library(
